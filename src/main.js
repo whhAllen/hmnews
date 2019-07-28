df7761a6881d979f8ccfd853313c2175b0cad7b6
whhallen <546432714@qq.com>
2019-07-28 16:41:37 +0800

    搭建项目

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import router from '@/router'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
